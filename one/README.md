7f7d889f58a044f7bebbfc77d553a6bef1f358fa
Mat Ryer <thatmatryer@gmail.com>
2015-08-21 11:39:04 +0100

    tidy up for release

--- one/README.md
+++
@@ -1,2 +0,0 @@
-# golanguk
-Source code for Golang UK talk - August 21st 2015, London.
--- two/routes.go
+++
@@ -1,7 +0,0 @@
-package main
-
-// START OMIT
-http.HandleFunc("/hello", handleHello)
-http.HandleFunc("/goodbye", handleGoodbye)
-http.HandleFunc("/", handleIndex)
-// END OMIT
